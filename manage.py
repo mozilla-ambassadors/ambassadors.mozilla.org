2494b5027e50559d4873977bdeb910a3cf0832b9
Erik Vold <erikvvold@gmail.com>
2010-10-14 19:54:14 -0700

    check for settings_local.py before checking for settings.py

--- manage.py
+++ manage.py
@@ -6,11 +6,17 @@ site.addsitedir('vendor')
 site.addsitedir('vendor/lib/python')
 
 try:
-    import settings # Assumed to be in the same directory.
+    import settings_local as settings
 except ImportError:
-    import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
-    sys.exit(1)
+    try:
+        import settings # Assumed to be in the same directory.
+    except ImportError:
+        import sys
+        sys.stderr.write(
+            "Error: Tried importing 'settings_local.py' and 'settings.py' "
+            "but neither could be found (or they're throwing an ImportError)."
+            " Please come back and try again later.")
+        raise
 
 if __name__ == "__main__":
     execute_manager(settings)
